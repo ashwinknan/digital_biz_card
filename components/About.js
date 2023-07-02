75bb3ce30766c0d434b71716534396c94771a611
Ashwin Krishnan <9294975+ashwinknan@users.noreply.github.com>
2023-07-02 17:49:28 +0530

    Small Spelling error

--- components/About.js
+++ components/About.js
@@ -4,7 +4,7 @@ export default function About(){
     return (
         <div className = "blurb">
             <h3>About</h3>
-            <p>I currently lead products for an ed-tech company. I love working at the intersection of education, policy and technology. Building educational apps from scratch excites me as much as teaching children in a classroom does.</p>
+            <p>I currently leading products for an ed-tech company. I love working at the intersection of education, policy and technology. Building educational apps from scratch excites me as much as teaching children in a classroom does.</p>
         </div>
         )
 }
